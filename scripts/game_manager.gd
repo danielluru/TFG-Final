ae4abe2d37d92f3218bdf0feda1706d21f4dbccc
danielluru <danielluru77@gmail.com>
2025-05-25 21:49:26 +0200

    añadimos plataformas y cambiamos todo el mapa

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,11 +1,7 @@
 extends Node2D
-@onready var score_label: Label = $ScoreLabel
 
 var score = 19
 var levelCoins = 20
 
 func add_point():
 	score += 1
-	##score_label.text = str(score) + " de " + str(levelCoins)
-	#if score == levelCoins:
-	#	score = 0
